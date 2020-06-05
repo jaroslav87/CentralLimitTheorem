0926a7f38ca4f8d3140325d7a9c85195ac5e45c3
jaroslav87 <j.goslinski@aisens.co>
2020-06-05 16:32:19 +0200

    graph corrections, alpha and labels

--- src/clt.py
+++ src/clt.py
@@ -12,9 +12,9 @@ s_mean_10 = np.mean(s10, 0)
 # create bins
 bins = np.linspace(0, 10, 1000)
 # plot histogram of one of the samples and distribuition of the mean
-plt.hist(s30[0, :], bins, alpha=0.6, label='chi-square, k=2')
-plt.hist(s_mean_10, bins, alpha=0.6, label='distribution of sample means, s=10')
-plt.hist(s_mean_30, bins, alpha=0.6, label='distribution of sample means, s=30')
+plt.hist(s30[0, :], bins, alpha=0.4, label='chi-square, k = 2')
+plt.hist(s_mean_10, bins, alpha=0.4, label='distribution of sample means, s = 10')
+plt.hist(s_mean_30, bins, alpha=0.4, label='distribution of sample means, s = 30')
 plt.legend(loc='upper right')
 plt.show()
 
